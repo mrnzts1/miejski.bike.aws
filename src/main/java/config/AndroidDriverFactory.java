6e0f849f872a12e1f89b3b2c4abb69acaf7c6f7f
mrnzts1 <mrnzts@gmail.com>
2019-02-07 19:42:40 +0100

    Android driver factory

--- src/main/java/config/AndroidDriverFactory.java
+++ src/main/java/config/AndroidDriverFactory.java
@@ -42,7 +42,7 @@ public class AndroidDriverFactory {
         capabilities.setCapability("platformName", "ANDROID");
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "PIXEL_2");
         capabilities.setCapability(CapabilityType.VERSION, "8.1.0");
-        capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
+        //capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
         capabilities.setCapability("deviceName", "PIXEL 2");
         capabilities.setCapability("app", "http://testinium-hub.s3.amazonaws.com/2461/automated-upload/miejski-bike-release-2.8.0-beta1-71f1872f.apk");
 
